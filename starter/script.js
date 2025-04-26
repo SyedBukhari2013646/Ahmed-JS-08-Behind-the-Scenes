170d2a7d6f1727040ddf084d8e7365b5153d4fec
SyedBukhari2013646 <syedbukhari03249334041@gmail.com>
2025-04-26 20:06:32 +0500

    Hey there! i did put some more code about more functionality in this code so there it is...

--- starter/script.js
+++ starter/script.js
@@ -33,47 +33,111 @@
 // const firstName = 'Ahmed';
 // console.log(calcAge(1991));
 
-//Variables
-console.log(me);
-// console.log(job);
-// console.log(year);
+// //Variables
+// console.log(me);
+// // console.log(job);
+// // console.log(year);
 
-var me = 'Ahmed';
-let job = 'Web Developer';
-const year = 2001;
+// var me = 'Ahmed';
+// let job = 'Web Developer';
+// const year = 2001;
 
-// functions
+// // functions
 
-console.log(addDecl(2, 3));
-// console.log(addExp(2, 3));
-// console.log(addArrow(2, 3));
-function addDecl(a, b) {
-  return a + b;
-}
-
-// var addExp = function (a, b) {
+// console.log(addDecl(2, 3));
+// // console.log(addExp(2, 3));
+// // console.log(addArrow(2, 3));
+// function addDecl(a, b) {
 //   return a + b;
+// }
+
+// // var addExp = function (a, b) {
+// //   return a + b;
+// // };
+
+// // const addArrow = (a, b) => a + b;
+
+// // Example
+// console.log(undefined);
+
+// if (!numProducts) deleteShoppingCart();
+
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//   console.log('All Product Deleted');
+// }
+
+// // Some windows Object
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z);
+
+// This keyword
+
+// console.log(this);
+
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   console.log(this);
 // };
 
-// const addArrow = (a, b) => a + b;
+// calcAge(2002);
 
-// Example
-console.log(undefined);
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
 
-if (!numProducts) deleteShoppingCart();
+// calcAgeArrow(2008);
 
-var numProducts = 10;
+// const ahmed = {
+//   year: 2002,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
+//   },
+// };
 
-function deleteShoppingCart() {
-  console.log('All Product Deleted');
-}
+// ahmed.calcAge();
 
-// Some windows Object
+// const aqsa = {
+//   year: 2004,
+// };
 
-var x = 1;
-let y = 2;
-const z = 3;
+// aqsa.calcAge = ahmed.calcAge;
 
-console.log(x === window.x);
-console.log(y === window.y);
-console.log(z === window.z);
+// aqsa.calcAge();
+
+// const f = ahmed.calcAge;
+// f();
+
+// This keyword On Arrow And Regular functions
+
+// var firstName = 'Aqsa';
+
+const ahmed = {
+  firstName: 'Ahmed',
+  year: 2002,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+
+  greet: () => {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+
+  greet: function () {
+    console.log(this);
+    console.log(`Hey ${this.firstName}`);
+  },
+};
+
+ahmed.greet();
